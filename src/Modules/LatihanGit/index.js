c16721a718ee9e03094ea8cdd3e755689dc6e138
mochammadilham06 <stormsykres1006@gmail.com>
2022-08-23 13:49:23 +0700

    Initial Developer Branch

--- src/Modules/LatihanGit/index.js
+++ src/Modules/LatihanGit/index.js
@@ -4,6 +4,7 @@ import React from 'react';
 class ShoppingList extends React.Component {
   render() {
     return (
+      // Developer Branch
       <div className="shopping-list">
         <h1>Shopping List for {this.props.name}</h1>
         <ul>
